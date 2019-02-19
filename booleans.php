441a749ae26d3713ac4a98f5a22abe181a3cbc0f
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-02-19 13:51:25 +0100

    practiced PHP booleans

---
+++ booleans.php
@@ -0,0 +1,30 @@
+<?php
+
+// echo true; //outputs 1;
+// echo false; //outputs "";
+
+// //NUMBERS
+
+// echo 5 < 10; //outputs 1 which means true;
+// echo 5 == 10; //outputs " " which means false;
+// echo 10 == 10;//outputs 1
+// echo 5 != 10;//outputs 1
+// echo 5<=5 ; //outputs 1
+// echo 5 >=5; //outputs 1
+
+//STRINGS
+// echo "ebuka" < "yoshi"; //outputs 1; 
+// echo "ebuka" > "Ebuka"; //outputs 1; 
+// echo "ebuka" == "ebuka"; //outputs 1; 
+// echo "ebuka" == "Ebuka"; //outputs "";
+
+//lose versus strict comparisons
+
+// echo "5" == 5; //outputs 1; 
+// echo "5" === 5; //outputs ""; 
+
+
+
+
+
+?>
